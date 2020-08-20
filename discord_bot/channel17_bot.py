99fceb7c40ad281a9c56990b6aa6d96253491838
Drake Thomas <drake550@pm.me>
2020-08-19 20:59:03 -0400

    working, checks for new every 12h, .ga command works

--- discord_bot/channel17_bot.py
+++ discord_bot/channel17_bot.py
@@ -1,38 +1,38 @@
 import discord
 from discord.ext import commands, tasks
-
 from itertools import cycle
 import asyncio
 from datetime import datetime
-
 import scripts.get_agendas as ga
-
-def sprint(msg):
-    print(datetime.today(), '|', msg)
-    
 
 with open('/home/slime/channel17_bot_token.txt', 'r') as file:
     TOKEN = file.readline()
 
+def sprint(msg):
+    print(datetime.today(), '|', msg)
 
 if not TOKEN:
     print("No Token")
     exit()
 
 bot = commands.Bot(command_prefix='.')
+# Channel to for check_new_agenda loop
+update_channel = None
+# Latest agenda discord embed object
+latest_agenda = None
 
 @bot.event
 async def on_ready():
-    print("Bot Connected.")
+    global update_channel
+    sprint("Bot Connected.")
+    await bot.wait_until_ready()
+    update_channel = bot.get_channel(745699017811296319)
+    check_new_agendas.start()
 
-@tasks.loop(seconds=5.0)
-async def check_new_agendas():
-    sprint("Checking for new agendas")
-    if ga.check_new():
-        sprint("New agenda found.")
-        fetch_agendas()
 
+# Fetches latest agenda and puts in discord embed
 async def fetch_agendas():
+    global latest_agenda
     agenda = ga.get_agendas()[-1]
     embed = discord.Embed(
             title = 'Latest Agenda',
@@ -41,13 +41,23 @@ async def fetch_agendas():
     embed.add_field(name='Link:', value=agenda[2])
 
     sprint("Fetching latest Sandown agenda")
-    await ctx.send('Fetching...')
-    await ctx.send(embed=embed)
+    latest_agenda = embed
 
+# Checks if new agenda has been added.
+@tasks.loop(hours=12)
+async def check_new_agendas():
+    sprint("Checking for new agendas")
+    if ga.check_new():
+        sprint("New agenda found.")
+        await fetch_agendas()
+        await update_channel.send(embed=latest_agenda)
+
+# Show latest agenda.
 @bot.command(aliases=['ga', 'agenda', 'latest agenda'])
 async def latest_agenda(ctx):
-    fetch_agendas()
+    await ctx.send('Fetching...')
+    await fetch_agendas()
+    await ctx.send(embed=latest_agenda)
 
 
-check_new_agendas.start()
 bot.run(TOKEN)
