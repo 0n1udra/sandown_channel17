1c614b9787689fcbf379e8ee3ca62d13ea966828
0n1udra <dt01@pm.me>
2021-11-08 14:07:46 -0500

    updated startup tmux setup script
    removed try and except statements, add small pauses (need the small pauses)

--- discord_bot/scripts/start_sandown_bot.py
+++ discord_bot/scripts/start_sandown_bot.py
@@ -1,19 +1,20 @@
 import time, os
 
-def setup_tmux():
-    try:
-        os.system('tmux send-keys -t mcserver:1.1 "tmux split-window -h" ENTER')
-        print("Split lower pane horizontally.")
-    except:
-        print("Error splitting window.")
-    
-    try:
-        os.system('tmux send-keys -t mcserver:1.2 "sandownbot" ENTER')
-        print("Started sandown bot.")
-    except:
-        print("Error starting sandown bot.")
+def start_tmux():
+    """Starts detached Tmux session, with 2 panes, named 'sess'."""
 
+    os.system('tmux new -d -s sess')
+    os.system('tmux send-keys -t sess:1.0 "tmux split-window -v" ENTER')
+    time.sleep(.5)
+    os.system('tmux send-keys -t sess:1.0 "tmux new-window" ENTER')
+    time.sleep(.5)
+    os.system('tmux send-keys -t sess:2.0 "tmux split-window -v" ENTER')
     time.sleep(1)
 
+
+def setup_tmux():
+    os.system('tmux send-keys -t sess:1.1 "sandownbot" ENTER')
+
 if __name__ == '__main__':
+    start_tmux()
     setup_tmux()
